7b4456068382563389113b9fccc0454c4b20772d
Mr-ironniu <751635193@qq.com>
2020-05-05 00:12:41 +0800

    ..

--- app/src/views/Home/message/message.vue
+++ app/src/views/Home/message/message.vue
@@ -1,22 +1,42 @@
 <template>
     <div>
-        1231
+        <div class="publish">
+            <img  src="../../../assets/xiangji.png" alt="">
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     name:'message',
-    data(){
-        return{
-            data:[],
-            active:''
-        }
-    }
-    
+     data() {
+        return {
+        loading: true,
+        };
+    },
+    mounted() {
+        this.loading = false;
+    },
 }
 </script>
 
 <style  scoped>
-
+.publish{
+    height: 40px;
+    width:40px;
+    position: fixed;
+    right:0;
+    top:0;
+    margin: auto;
+}
+.publish img{
+    height:25px;
+    width:25px;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right:0;
+    left:0;
+    margin: auto;
+}
 </style>
